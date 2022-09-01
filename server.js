b499b9ed093a8bd10fc2015a18b22716e3c5e5d9
npsaunders <neilpsaunders@gmail.com>
2022-09-01 12:43:39 -0400

    initial express setup

---
+++ server.js
@@ -0,0 +1,23 @@
+// VARIABLES
+const express = require("express")
+const app = express();
+const port = 3000;
+
+
+// MIDDLEWARE
+
+
+
+// ROUTES
+// I NDEX
+// N EW
+// D ELETE
+// U PDATE
+// C REATE
+// E DIT
+// S HOW
+
+
+app.listen(port, () => {
+  console.log("Listening...port=", port);
+})
